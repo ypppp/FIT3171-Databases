194e23c94db9982a15b5ae1db2e612916260fdbd
Yee Perng Yew <yyew0004@student.monash.edu>
2023-10-12 16:03:58 +0800

    Created the tables for EMERGENCY_CONTACT, PATIENT and APPOINTMENT

--- Assignments/Ass2A/T1-mns-schema.sql
+++ Assignments/Ass2A/T1-mns-schema.sql
@@ -1,10 +1,10 @@
 --*****PLEASE ENTER YOUR DETAILS BELOW*****
 --T1-mns-schema.sql
 
---Student ID:
---Student Name:
---Unit Code:
---Applied Class No:
+--Student ID: 32205481
+--Student Name: Yee Perng Yew
+--Unit Code: FIT3171
+--Applied Class No: 02
 
 /* Comments for your marker:
 
@@ -17,15 +17,52 @@
 -- Ensure all column comments, and constraints (other than FK's)
 -- are included. FK constraints are to be added at the end of this script
 
+DROP TABLE appointment CASCADE CONSTRAINTS;
+
+DROP TABLE emergency_contact CASCADE CONSTRAINTS;
+
+DROP TABLE patient CASCADE CONSTRAINTS;
+
 -- TABLE: APPOINTMENT
 
+CREATE TABLE appointment (
+    appt_no           NUMBER(7) NOT NULL,
+    appt_datetime     DATE NOT NULL,
+    appt_roomno       NUMBER(2) NOT NULL,
+    appt_length       CHAR(1) NOT NULL,
+    patient_no        NUMBER(4) NOT NULL,
+    provider_code     CHAR(6) NOT NULL,
+    nurse_no          NUMBER(3) NOT NULL,
+    appt_prior_apptno NUMBER(7)
+);
 
 
 -- TABLE: EMERGENCY_CONTACT
 
+CREATE TABLE emergency_contact (
+    ec_id    NUMBER(4) NOT NULL,
+    ec_fname VARCHAR(30),
+    ec_lname VARCHAR(30),
+    ec_phone CHAR(10) NOT NULL
+);
+
 
 
 -- TABLE: PATIENT
+
+CREATE TABLE patient (
+    patient_no            NUMBER(4) NOT NULL,
+    patient_fname         VARCHAR(30),
+    patient_lname         VARCHAR(30),
+    patient_street        VARCHAR(50) NOT NULL,
+    patient_city          VARCHAR(20) NOT NULL,
+    patient_state         VARCHAR(3) NOT NULL,
+    patient_postcode      CHAR(4) NOT NULL,
+    patient_dob           DATE NOT NULL,
+    patient_contactmobile CHAR(10) NOT NULL,
+    patient_contactmail   VARCHAR(25) NOT NULL,
+    ec_id                 NUMBER(4) NOT NULL
+);
 
 
 
