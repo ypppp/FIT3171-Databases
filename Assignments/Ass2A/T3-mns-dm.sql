b45cd8468b2bc103e41ad78c1496723bce70ab43
Yee Perng Yew <yyew0004@student.monash.edu>
2023-10-13 22:41:47 +0800

    Inserted data required for 3b in Task 3

--- Assignments/Ass2A/T3-mns-dm.sql
+++ Assignments/Ass2A/T3-mns-dm.sql
@@ -30,6 +30,62 @@ CREATE SEQUENCE appointment_seq START WITH 100 INCREMENT BY 5;
 
 --3(b)
 
+    INSERT INTO emergency_contact VALUES (
+        0001,
+        'Jonathan',
+        'Robey',
+        '0412523122'
+);
+
+INSERT INTO patient VALUES (
+    1001,
+    'Laura',
+    'Robey',
+    '45 Chester St',
+    'Melbourne',
+    'VIC',
+    '3166',
+    TO_DATE('23-Sep-2010', 'dd-Mon-yyyy'),
+    '0395683868',
+    'Laurarobey@gmail.com',
+    0001
+);
+
+INSERT INTO patient VALUES (
+    1002,
+    'Lachlan',
+    'Robey',
+    '45 Chester St',
+    'Melbourne',
+    'VIC',
+    '3166',
+    TO_DATE('23-Nov-2012', 'dd-Mon-yyyy'),
+    '0397648564',
+    'Lachlanrobey@gmail.com',
+    0001
+);
+
+Insert into appointment values (
+    1000001,
+    TO_DATE('04-Sep-2023 15:30', 'dd-Mon-yyyy hh24:mi'),
+    02,
+    'S',
+    1001,
+    'GEN001',
+    006,
+    NULL  
+);
+
+Insert into appointment values (
+    1000002,
+    TO_DATE('04-Sep-2023 16:00', 'dd-Mon-yyyy hh24:mi'),
+    02,
+    'S',
+    1002,
+    'GEN001',
+    006,
+    NULL  
+);
 
 --3(c)
 
