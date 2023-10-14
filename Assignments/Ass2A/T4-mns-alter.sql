ce6e78b65daf00d159a3ffda731e1477ffac4676
Yee Perng Yew <yyew0004@student.monash.edu>
2023-10-14 12:51:39 +0800

    Did sql as required for 4a in task 4

--- Assignments/Ass2A/T4-mns-alter.sql
+++ Assignments/Ass2A/T4-mns-alter.sql
@@ -1,10 +1,10 @@
 --*****PLEASE ENTER YOUR DETAILS BELOW*****
 --T4-mns-alter.sql
 
---Student ID:
---Student Name:
---Unit Code:
---Applied Class No:
+--Student ID: Yee Perng Yew
+--Student Name: 32205481
+--Unit Code: FIT3171
+--Applied Class No: 02
 
 /* Comments for your marker:
 
@@ -14,6 +14,16 @@
 */
 
 --4(a)
+
+SELECT
+patient_no,
+COUNT(*) AS number_of_appointments
+FROM
+appointment
+GROUP BY
+patient_no
+ORDER BY
+patient_no;
 
 
 --4(b)
