94744d66c2d7cf47e5310e404cee1ae80432e417
Yee Perng Yew <yyew0004@student.monash.edu>
2023-10-27 02:03:21 +0800

    Completed task 2a sql

--- Assignments/Ass2B/T2-mns-select.sql
+++ Assignments/Ass2B/T2-mns-select.sql
@@ -1,10 +1,10 @@
 --*****PLEASE ENTER YOUR DETAILS BELOW*****
 --T2-mns-select.sql
 
---Student ID:
---Student Name:
---Unit Code:
---Applied Class No:
+--Student ID: 32205481
+--Student Name: Yee Perng Yew
+--Unit Code: FIT3171
+--Applied Class No: 02
 
 /* Comments for your marker:
 
@@ -18,12 +18,37 @@
 -- ENSURE that your query is formatted and has a semicolon
 -- (;) at the end of this answer
 
+SELECT
+    *
+FROM
+    mns.item
+WHERE item_stock >= 50
+AND item_desc LIKE '%composite%'
+order by item_stock DESC;
+
+
+
 
 
 /*2(b)*/
 -- PLEASE PLACE REQUIRED SQL SELECT STATEMENT FOR THIS PART HERE
 -- ENSURE that your query is formatted and has a semicolon
 -- (;) at the end of this answer
+
+SELECT
+    mns.spec_id
+FROM
+    mns.provider
+    JOIN mns.specialisation
+    on provider.spec_id = specialisation.spec_id
+WHERE
+    spec_name = 'PAEDIATRIC
+DENTISTRY'
+ORDER BY
+    provider_lname,
+    provider_fname,
+    provider_code;
+
 
 
 /*2(c)*/
