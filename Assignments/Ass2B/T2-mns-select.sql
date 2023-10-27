24a0b6f1bc8aadeff8a09f06a3c8fe1fc0c0c7f5
Yee Perng Yew <yyew0004@student.monash.edu>
2023-10-27 22:49:13 +0800

    completed task 2c sql

--- Assignments/Ass2B/T2-mns-select.sql
+++ Assignments/Ass2B/T2-mns-select.sql
@@ -61,7 +61,11 @@ ORDER BY
 -- (;) at the end of this answer
 
 SELECT
-    service_code, service_desc, '$' || service_stdfee
+    service_code,
+    service_desc,
+    lpad(to_char(service_stdfee, '$9999.99'),
+         14,
+         ' ') AS service_stdfee
 FROM
     mns.service
 WHERE
@@ -81,6 +85,35 @@ ORDER BY
 -- PLEASE PLACE REQUIRED SQL SELECT STATEMENT FOR THIS PART HERE
 -- ENSURE that your query is formatted and has a semicolon
 -- (;) at the end of this answer
+
+Select a.appt_no,
+    a.appt_datetime,
+    p.patient_no,
+    p.patient_fname
+    || ' '
+    || p.patient_lname                         AS patient_name,
+    lpad(to_char(( s.apptserv_fee + s.apptserv_itemcost ), '$9999.99'), 22, ' ') AS appointment_total_cost
+FROM
+         mns.appointment a
+    JOIN mns.patient   p
+    ON a.patient_no = p.patient_no
+    JOIN mns.appt_serv s
+    ON a.appt_no = s.appt_no
+GROUP BY
+    a.appt_no, a.appt_datetime, p.patient_no, p.patient_fname, p.patient_lname, s.apptserv_fee, s.apptserv_itemcost
+Having
+    max(s.apptserv_fee + s.apptserv_itemcost) =
+(SELECT
+    MAX(sum(s.apptserv_fee) + sum(s.apptserv_itemcost))
+FROM
+         mns.appointment a
+    JOIN mns.appt_serv s
+    ON a.appt_no = s.appt_no
+GROUP BY
+    s.apptserv_fee,
+    s.apptserv_itemcost)
+ORDER BY
+    a.appt_no;
 
 
 /*2(e)*/
